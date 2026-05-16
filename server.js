ff10d90477e9b99685e7e4322c8a201f911a4923
swaitankjha <swaitankj@gmail.com>
2026-05-16 20:33:21 +0530

    Added login feature

--- server.js
+++ server.js
@@ -15,25 +15,51 @@ const io = new Server(server, {
 });
 
 app.get("/", (req, res) => {
-    res.send("Server is running");
+    res.send("Server running");
 });
+
+const users = {};
 
 io.on("connection", (socket) => {
 
-    console.log("User connected");
+    console.log("User connected:", socket.id);
+
+    socket.on("register_user", (username) => {
+
+        users[username] = socket.id;
+
+        console.log(username + " registered");
+    });
 
     socket.on("send_message", (data) => {
 
-        console.log(data);
+        const targetSocketId = users[data.receiver];
 
-        io.emit("receive_message", data);
+        if (targetSocketId) {
+
+            io.to(targetSocketId).emit(
+                "receive_message",
+                data
+            );
+        }
     });
 
     socket.on("disconnect", () => {
-        console.log("User disconnected");
+
+        for (let username in users) {
+
+            if (users[username] === socket.id) {
+
+                delete users[username];
+            }
+        }
+
+        console.log("Disconnected:", socket.id);
     });
 });
 
-server.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log("Server running");
 });
